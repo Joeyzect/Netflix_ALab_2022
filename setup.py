2aa7726150e5229b3bc1990b5f120d008b0dc840
jyl2fb <jyl2fb@virginia.edu>
2022-10-05 13:15:03 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Project for ALab Fall 2022, Predicting top 10 movies/tv shows',
+    author='Joseph Lu',
+    license='MIT',
+)
